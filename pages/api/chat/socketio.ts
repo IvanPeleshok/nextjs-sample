f70a9665704411a5f464ecfb36ce9f6cb87b6115
Ivan Peleshok <peleshok.ivan@gmail.com>
2022-04-13 01:48:28 +0300

    some fix

--- pages/api/chat/socketio.ts
+++ pages/api/chat/socketio.ts
@@ -11,11 +11,13 @@ export const config = {
 
 export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
     if (!res.socket.server.io) {
-        const httpServer: NetServer = res.socket.server as any;
-        const io = new ServerIO(httpServer, {
-            path: "/api/chat/socketio",
-        });
-        res.socket.server.io = io;
+        // const httpServer: NetServer = res.socket.server as any;
+        // const io = new ServerIO(httpServer, {
+        //     path: "/api/chat/socketio",
+        // });
+        // res.socket.server.io = io;
+        const io = new ServerIO(res.socket.server as any as NetServer)
+        res.socket.server.io = io
     }
 
     res.end();
